61d5cbdaf0b65cfa877708d00a0862eef1ba5993
jonghwa <jonghwa0710@naver.com>
2019-02-16 23:57:51 +0900

    Initial commit

---
+++ src/main/java/me/jonghwa/demospringmvc/EventController.java
@@ -0,0 +1,22 @@
+package me.jonghwa.demospringmvc;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EventController {
+
+    @Autowired
+    EventService eventService;
+
+    @GetMapping("/events")
+    public String evnets(Model model) {
+        model.addAttribute("events", eventService.getEvents());
+
+        return "events";
+    }
+
+}

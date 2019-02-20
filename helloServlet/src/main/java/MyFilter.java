bfeb790054677195b15334ded4bec3469862dc21
jonghwa <jonghwa0710@naver.com>
2019-02-20 17:59:45 +0900

    listener & filter 적용확인 doinit -> filter -> doget 이후 filter -> doget

---
+++ helloServlet/src/main/java/MyFilter.java
@@ -0,0 +1,24 @@
+import sun.misc.Request;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class MyFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("Filter Init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("Filter");
+        // 다음 필터로 전달하기위해서는 체인을 연결해줘야함
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Filter Destroy");
+    }
+}
---
+++ helloServlet/src/main/java/MyListener.java
@@ -0,0 +1,17 @@
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class MyListener implements ServletContextListener {
+
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("context Initialized");
+        sce.getServletContext().setAttribute("name","jonghwa");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("context Destroyed");
+    }
+}
